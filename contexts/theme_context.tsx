c823ba6c840e5df094a7840c4c3c4ef0e559adb5
Ankur Gajurel <ankurgajurel02@gmail.com>
2023-09-17 15:28:54 +0545

    try

--- contexts/theme_context.tsx
+++ contexts/theme_context.tsx
@@ -1,33 +1,49 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 
 interface DarkModeContextProps {
   darkMode: boolean;
   toggleDarkMode: () => void;
 }
 
-const DarkModeContext = createContext<DarkModeContextProps | undefined>(undefined);
+const DarkModeContext = createContext<DarkModeContextProps | undefined>(
+  undefined
+);
 
 interface DarkModeProviderProps {
   children: ReactNode;
 }
 
-export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(true);
+export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({
+  children,
+}) => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode")
+  ); // Set the default value to false
 
   useEffect(() => {
-    const savedMode = localStorage.getItem('darkMode');
+    const savedMode = localStorage.getItem("darkMode");
     if (savedMode) {
       setDarkMode(JSON.parse(savedMode));
     }
   }, []);
 
+  // Use a useEffect to update the CSS class immediately
   useEffect(() => {
-    localStorage.setItem('darkMode', JSON.stringify(darkMode));
-    document.body.classList.toggle('dark', darkMode);
+    document.body.classList.toggle("dark", darkMode);
   }, [darkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode((prevMode) => !prevMode);
+    setDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", JSON.stringify(newMode)); // Update local storage
+      return newMode;
+    });
   };
 
   return (
@@ -40,7 +56,7 @@ export const DarkModeProvider: React.FC<DarkModeProviderProps> = ({ children }) 
 export const useDarkMode = () => {
   const context = useContext(DarkModeContext);
   if (!context) {
-    throw new Error('useDarkMode must be used within a DarkModeProvider');
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
   }
   return context;
 };
