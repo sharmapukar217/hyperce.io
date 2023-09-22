27342a44398e3398a8f82fcd50c09e2325521724
Ankur Gajurel <ankurgajurel02@gmail.com>
2023-09-22 14:27:25 +0545

    `fix`: forgot to commit

---
+++ data/HeadlessGallary.ts
@@ -0,0 +1,27 @@
+export const HeadlessGallary = [
+    {
+        img_name: "Maipic",
+        a_href: "https://maipic.hyperce.io/",
+        img_src: "/gallary/maipic_demo.png",
+    },
+    {
+        img_name: "Shopvest",
+        a_href: "https://trylah.sg/",
+        img_src: "/gallary/shopvest_demo.png",
+    },
+    {
+        img_name: "Store In House",
+        a_href: "https://storeinhouse.com.ar/home",
+        img_src: "/gallary/storeinhouse_demo.png",
+    },
+    {
+        img_name: "Menzz",
+        a_href: "https://menzz.co/",
+        img_src: "/gallary/menzz_demo.png",
+    },
+    {
+        img_name: "Goonji",
+        a_href: "https://goonji.vercel.app/",
+        img_src: "/gallary/goonji_demo.png",
+    },
+]
