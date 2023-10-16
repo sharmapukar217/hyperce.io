25ce0ca065f57a080cc1bc12584ea69a682d5895
Ankur Gajurel <ankurgajurel02@gmail.com>
2023-10-16 18:55:19 +0545

    `edit`: product name correction

--- app/ink/page.tsx
+++ app/ink/page.tsx
@@ -38,7 +38,7 @@ export default function Services() {
         demoEmail="demo@hyperce.io"
         demoPassword="@Hyperce123"
         qrSource="/ink.demo.qr.png"
-        productName="Frontier"
+        productName="Ink"
       />
       <WhyChoose />
       {/* <Gallary /> */}
