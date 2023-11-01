d1e4d61fd80ffe018c1f485f2cd51efeedef8d6f
Ankur Gajurel <ankurgajurel02@gmail.com>
2023-11-01 21:38:22 +0545

    fix proposal button

--- components/Navbar/Hamburger.tsx
+++ components/Navbar/Hamburger.tsx
@@ -7,7 +7,6 @@ import { BsFillCaretDownFill } from "react-icons/bs";
 import { usePathname } from "next/navigation";
 
 export default function Hamburger(props: any) {
-
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [isDropdownHovered, setIsDropdownHovered] = useState(false);
 
@@ -117,57 +116,96 @@ export default function Hamburger(props: any) {
                     key={menu.indexOf(eachMenu)}
                     className="lg:hover:scale-[105%] text-md transition-all duration-200 text-black dark:text-white hover:text-gray-500 hover:dark:text-gray-200"
                   >
-                    <a href={eachMenu.link} className="font-bold hover:dark:text-[#357D8A]">{eachMenu.title}</a>
+                    <a
+                      href={eachMenu.link}
+                      className="font-bold hover:dark:text-[#357D8A]"
+                    >
+                      {eachMenu.title}
+                    </a>
                   </li>
                 ))}
                 <a>
-              <div
-                onMouseEnter={showDropdown}
-                onMouseLeave={hideDropdown}
-                className="relative"
-              >
-                <li
-                  className={`hover:scale-[105%] flex inline-flex items-center justify-center text-base font-bold transition-all duration-200 cursor-pointer ${
-                    isDropdownVisible ? "text-[#337684]" : ""
-                  }`}
-                >
-                  <span className="flex inline-flex items-center justify-center">
-                  Solutions <BsFillCaretDownFill />
-                  </span>
-                </li>
-                {isDropdownVisible && (
                   <div
-                    onMouseEnter={() => setIsDropdownHovered(true)}
-                    onMouseLeave={() => setIsDropdownHovered(false)}
-                    className={`absolute w-60 z-50 bg-transparent  px-2 py-1 transition-all duration-500 animate ease-linear p-2 ${
-                      isDropdownVisible ? "dropdown-visible" : "dropdown-hidden"
-                    }`}
+                    onMouseEnter={showDropdown}
+                    onMouseLeave={hideDropdown}
+                    className="relative"
                   >
-                    {solutions.map((each) => (
-                      <>
-                        <a
-                          className="inline-flex text-xs gap-x-2  w-full items-center pl-2 py-1 text-gray-600 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-300"
-                          href={each.href}
-                        >
-                          <img
-                            className="h-8 w-8 hidden dark:block"
-                            src={each.darkIcon}
-                            alt=""
-                          />
-                          <img
-                            className="h-8 w-8 dark:hidden"
-                            src={each.whiteIcon}
-                            alt=""
-                          />
-                          {each.name}
-                        </a>
-                      </>
-                    ))}
+                    <li
+                      className={`hover:scale-[105%] flex inline-flex items-center justify-center text-base font-bold transition-all duration-200 cursor-pointer ${
+                        isDropdownVisible ? "text-[#337684]" : ""
+                      }`}
+                    >
+                      <span className="flex inline-flex items-center justify-center">
+                        Solutions <BsFillCaretDownFill />
+                      </span>
+                    </li>
+                    {isDropdownVisible && (
+                      <div
+                        onMouseEnter={() => setIsDropdownHovered(true)}
+                        onMouseLeave={() => setIsDropdownHovered(false)}
+                        className={`absolute bg-black w-60 z-50 bg-transparent  px-2 py-1 transition-all duration-500 animate ease-linear p-2 ${
+                          isDropdownVisible
+                            ? "dropdown-visible"
+                            : "dropdown-hidden"
+                        }`}
+                      >
+                        {solutions.map((each) => (
+                          <>
+                            <a
+                              className="inline-flex text-xs gap-x-2  w-full items-center pl-2 py-1 text-gray-600 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-300"
+                              href={each.href}
+                            >
+                              <img
+                                className="h-8 w-8 hidden dark:block"
+                                src={each.darkIcon}
+                                alt=""
+                              />
+                              <img
+                                className="h-8 w-8 dark:hidden"
+                                src={each.whiteIcon}
+                                alt=""
+                              />
+                              {each.name}
+                            </a>
+                          </>
+                        ))}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            </a>
+                </a>
               </ul>
+              <a target="_blank" href="https://docs.hyperce.io/proposal.pdf" className="mt-10">
+                <button
+                  aria-label="proposal-download"
+                  className="flex h-full gap-1 hover:scale-[105%] active:scale-[95%] transition-all duration-300 text-white bg-[#357D8A] px-5 py-1 rounded-full items-center"
+                >
+                  <div className="svg">
+                    <svg
+                      width="20px"
+                      height="20px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
+                        stroke="currentColor"
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                      <path
+                        d="M12 3V16M12 16L16 11.625M12 16L8 11.625"
+                        stroke="currentColor"
+                        strokeWidth="3"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </svg>
+                  </div>
+                  <span className="text-base font-bold">Proposal</span>
+                </button>
+              </a>
             </div>
           </div>
         </div>
