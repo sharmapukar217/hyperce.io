8706b49c0a4da22d40ab61849d594e7a408a3f4c
Pukar Sharma <sharmapukar217@gmail.com>
2024-02-16 09:34:45 +0545

    Update useFounderData.tsx

--- lib/useFounderData.tsx
+++ lib/useFounderData.tsx
@@ -11,7 +11,8 @@ export const useFounderData = () => {
   const [data, setData] = useState({
     title: 'Founder & CEO, Hyperce.',
     founder: 'Biplab Karki',
-    imageUrl: '/biplab.png'
+    imageUrl: '/biplab.png',
+    quote: "As the founder of Hyperce, I am driven by a vision of a dynamic eCommerce landscape where businesses thrive, innovate, and succeed. We are here to empower your journey with cutting-edge eCommerce platforms and AI-powered solutions. Together, we'll shape the future of online commerce. Welcome to the Hyperce family!"
   });
 
   useEffect(() => {
@@ -23,7 +24,8 @@ export const useFounderData = () => {
           founder: 'Huw Davies',
           title: 'CEO, Hyperce UK',
           imageUrl:
-            'https://media.discordapp.net/attachments/1129807618907590656/1206929912116682824/huwd.png?ex=65ddcc12&is=65cb5712&hm=f7e20df1890216a63f6ae55f09306f668bf1655d154b79a99387363837d67a2b&=&format=webp&quality=lossless&width=575&height=603'
+            'https://media.discordapp.net/attachments/1129807618907590656/1206929912116682824/huwd.png?ex=65ddcc12&is=65cb5712&hm=f7e20df1890216a63f6ae55f09306f668bf1655d154b79a99387363837d67a2b&=&format=webp&quality=lossless&width=575&height=603',
+         quote: "As the founder and CEO of Hyperce UK, I am driven by a vision of a dynamic eCommerce landscape where businesses thrive, innovate, and succeed. We are here to empower your journey with cutting-edge eCommerce platforms and AI-powered solutions. Together, we'll shape the future of online commerce. Welcome to the Hyperce family!"
         });
         break;
       }
@@ -31,7 +33,8 @@ export const useFounderData = () => {
         setData({
           founder: 'Biplab Karki',
           title: 'Founder & CEO, Hyperce.',
-          imageUrl: '/biplab.png'
+          imageUrl: '/biplab.png',
+          quote: "As the founder of Hyperce, I am driven by a vision of a dynamic eCommerce landscape where businesses thrive, innovate, and succeed. We are here to empower your journey with cutting-edge eCommerce platforms and AI-powered solutions. Together, we'll shape the future of online commerce. Welcome to the Hyperce family!"
         });
         break;
       }
