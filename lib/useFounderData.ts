96a99561808a16393dc055e4c7f8ae045399be90
Pukar Sharma <sharmapukar217@gmail.com>
2024-02-21 09:39:17 +0545

    Update useFounderData.ts

--- lib/useFounderData.ts
+++ lib/useFounderData.ts
@@ -30,7 +30,7 @@ export const useFounderData = () => {
       case Domain.HyperceAu: {
         setData({
           founder: 'Jawwad Aamer Khan',
-          title: 'CEO, Hyperce Australia',
+          title: 'CEO, Hyperce',
           imageUrl:
             'https://media.discordapp.net/attachments/1129807618907590656/1208043723418632242/WhatsApp_Image_2024-02-16_at_19.16.28.jpeg?ex=65e1d964&is=65cf6464&hm=742ed45a9bf082210a2945a7ab7bfa2423ce047bfb92c81d7dcb9a2682b409ea&=&width=575&height=603',
           quote:
