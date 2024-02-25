b1ed7335075166b22be69fcfdffe2cb492775d24
Pukar Sharma <sharmapukar217@gmail.com>
2024-02-25 23:16:21 +0545

    fixing bod page

--- app/bod/page.tsx
+++ app/bod/page.tsx
@@ -98,7 +98,7 @@ export default function BOD() {
             {boardOfDirectors.map((bod, idx) => (
               <div
                 key={idx}
-                className="relative max-w-none rounded-xl h-96 w-96 border-2 dark:border-gray-700 cursor-pointer shadow-sm group overflow-clip mx-auto"
+                className="relative max-w-none rounded-xl h-96 w-96 border-2 dark:border-gray-700 cursor-pointer shadow-md group overflow-clip mx-auto"
               >
                 <img
                   key={idx}
