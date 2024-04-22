29c80b7a541c032568cdbb49d8989f7212e7a502
Pukar Sharma <sharmapukar217@gmail.com>
2024-04-23 01:53:05 +0545

    fixing founder images

--- lib/useFounderData.ts
+++ lib/useFounderData.ts
@@ -20,8 +20,7 @@ export const useFounderData = () => {
         setData({
           founder: 'Huw Davies',
           title: 'CEO, Hyperce UK',
-          imageUrl:
-            'https://media.discordapp.net/attachments/1129807618907590656/1206929912116682824/huwd.png?ex=65ddcc12&is=65cb5712&hm=f7e20df1890216a63f6ae55f09306f668bf1655d154b79a99387363837d67a2b&=&format=webp&quality=lossless&width=575&height=603',
+          imageUrl: '/Huw Davies.webp',
           quote:
             "As the founder and CEO of Hyperce UK, I am driven by a vision of a dynamic eCommerce landscape where businesses thrive, innovate, and succeed. We are here to empower your journey with cutting-edge eCommerce platforms and AI-powered solutions. Together, we'll shape the future of online commerce. Welcome to the Hyperce family!"
         });
@@ -31,8 +30,7 @@ export const useFounderData = () => {
         setData({
           founder: 'Jawwad Aamer Khan',
           title: 'CEO, Hyperce',
-          imageUrl:
-            'https://media.discordapp.net/attachments/1129807618907590656/1208043723418632242/WhatsApp_Image_2024-02-16_at_19.16.28.jpeg?ex=65e1d964&is=65cf6464&hm=742ed45a9bf082210a2945a7ab7bfa2423ce047bfb92c81d7dcb9a2682b409ea&=&width=575&height=603',
+          imageUrl: '/Jawwad Aamer Khan.jpeg',
           quote:
             "As the founder and CEO of Hyperce Australia, I am driven by a vision of a dynamic eCommerce landscape where businesses thrive, innovate, and succeed. We are here to empower your journey with cutting-edge eCommerce platforms and AI-powered solutions. Together, we'll shape the future of online commerce. Welcome to the Hyperce family!"
         });
