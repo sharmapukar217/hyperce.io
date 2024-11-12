517c9e716ed8d8db69ee9d3c8435289ceb056e7c
Pukar Sharma <sharmapukar217@gmail.com>
2024-11-12 05:03:46 +0000

    style: add space after workplace: string

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -185,7 +185,7 @@ function EachOffice({ contactAddressDetails }: EachOfficeProps) {
                   <path d="M3 22v-2c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2H3Z" />
                 </svg>
               </div>
-              <div className="place dark:text-[#9CA3AF] text-base space-y-1">
+              <div className="place dark:text-[#9CA3AF] text-base space-x-1">
                 <span>Workplace:</span>
                 <span
                   dangerouslySetInnerHTML={{
