23866a7f89fef7d089e5a967cbbbd125220268e3
Pukar Sharma <sharmapukar217@gmail.com>
2025-05-28 15:49:47 +0545

    Update page.tsx

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -135,7 +135,7 @@ type PageProps = {
   searchParams?: { query?: string; page?: string };
 };
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 export default async function Page(props: PageProps) {
