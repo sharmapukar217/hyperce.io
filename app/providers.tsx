ce3359d9001f31a495ff242e1e8ff50c39e379a9
Pukar Sharma <sharmapukar217@gmail.com>
2025-06-29 23:43:56 +0545

    CHORE(chatbot): disable chatbot on prod

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,11 +4,6 @@ import * as React from 'react';
 import { ThemeProvider } from 'next-themes';
 
 export default function Providers(props: React.PropsWithChildren) {
-  React.useEffect(() => {
-    if (typeof window.crypto.randomUUID === 'undefined') {
-      window.crypto.randomUUID = () => '';
-    }
-  }, []);
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       {props.children}
