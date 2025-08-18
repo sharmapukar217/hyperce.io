cd20ae6b332316756d037a7c5bd8d0beebc8906a
Pukar Sharma <sharmapukar217@gmail.com>
2025-08-18 22:45:32 +0545

    ...hide changes

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -87,7 +87,7 @@ export default function Navbar(props: any) {
               </button>
             </Link>*/}
 
-            <Link
+            {/*<Link
               href="/#exclusive-offers"
               className="flex items-center justify-center h-full gap-2 hover:scale-[105%] active:scale-[95%] transition-all duration-300 text-white bg-blue-500 px-5 py-2 rounded-full"
             >
@@ -103,7 +103,7 @@ export default function Navbar(props: any) {
                   <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A3 3 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43zM9 3h2.932l.023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zm6 4v7.5a1.5 1.5 0 0 1-1.5 1.5H9V7zM2.5 16A1.5 1.5 0 0 1 1 14.5V7h6v9z" />
                 </svg>
               </div>
-            </Link>
+            </Link>*/}
 
             <Link href={proposalLink} target="_blank">
               <button
