888caab13d2f4d2e806d079dd77782cba3f4411a
Pukar Sharma <sharmapukar217@gmail.com>
2025-08-31 12:01:32 +0000

    ...

--- app/icon/route.tsx
+++ app/icon/route.tsx
@@ -17,14 +17,24 @@ const loadIconFromIconify = async (iconName: string | null) => {
   }
 };
 
+// Helper function to modify the color of the SVG content
+const changeSvgColor = (svgContent: string, color: string) => {
+  return svgContent.replace(/fill="([^"]*)"/g, `fill="${color}"`);
+};
+
 export const runtime = 'nodejs';
 
 export async function GET(request: NextRequest) {
   const iconName = request.nextUrl.searchParams.get('icon');
+  const color = request.nextUrl.searchParams.get('color') || '#000000'; // Default to black if no color is provided
+
   const iconData = await loadIconFromIconify(iconName);
 
   if (iconData) {
-    const pngBuffer = await sharp(Buffer.from(iconData))
+    const svgData =
+      color !== '#000000' ? changeSvgColor(iconData, color) : iconData;
+
+    const pngBuffer = await sharp(Buffer.from(svgData))
       .resize(128, 128, {
         fit: 'contain',
         background: { r: 0, g: 0, b: 0, alpha: 0 }
@@ -32,16 +42,17 @@ export async function GET(request: NextRequest) {
       .png()
       .toBuffer();
 
+    // @ts-expect-error
     return new Response(pngBuffer, {
       headers: {
-        'Content-Type': 'image/webp',
+        'Content-Type': 'image/png',
         'Cache-Control': 'public, max-age=31536000, immutable'
       }
     });
   }
 
   return NextResponse.json(
-    { message: `icon '${iconName}' not found.` },
+    { message: `Icon '${iconName}' not found.` },
     { status: 404 }
   );
 }
